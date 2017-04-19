23db69328da2aaf92d782e322a6b07bed140fdb3
AlexMorrison <amo37066@uga.edu>
2017-04-19 09:24:26 -0400

    Add basic database operations

--- STEPS/src/com/steps/persistencelayer/DbAccess.java
+++ STEPS/src/com/steps/persistencelayer/DbAccess.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbAccess {
 	
@@ -14,6 +13,11 @@ public class DbAccess {
 	static final String DB_CONNECTION_USERNAME = "root";
 	static final String DB_CONNECTION_PASSWORD = "root";
 	
+	/**
+	 * Opens a new connection to the database.
+	 * 
+	 * @return the connection object
+	 */
 	public Connection connect() {
 		Connection con = null;
 		try {
@@ -26,5 +30,81 @@ public class DbAccess {
 		}
 		return con;
 	} // connect
+	
+	/**
+	 * Executes a retrieve based SQL query such as SELECT.
+	 * Query must be in the form of a PreparedStatement
+	 * 
+	 * @param connection	database connection object
+	 * @param query			query to be executed
+	 * @return				ResultSet containing results of query
+	 */
+	public ResultSet retrieve(Connection connection, PreparedStatement query) {
+		ResultSet results = null;
+		try {
+			results = query.executeQuery();
+			return results;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		return results;
+	} // retrieve
+	
+	/**
+	 * Executes an insert based SQL query such as INSERT INTO.
+	 * Query must be in the form of a PreparedStatement.
+	 * 
+	 * @param connection	database connection object
+	 * @param query			query to be executed
+	 * @return				number of rows affected
+	 */
+	public int insert(Connection connection, PreparedStatement query) {
+		int rowsAffected = 0;
+		try {
+			rowsAffected = query.executeUpdate();
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return rowsAffected;
+	} // insert
+	
+	/**
+	 * Executes an update based SQL query such as UPDATE.
+	 * Query must be in the form of a PreparedStatment.
+	 * 
+	 * @param connection	database connection object
+	 * @param query			query to be executed
+	 * @return				number of rows affected
+	 */
+	public int update(Connection connection, PreparedStatement query) {
+		int rowsAffected = 0;
+		try {
+			rowsAffected = query.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return rowsAffected;
+	} // update
+	
+	/**
+	 * Executes a delete based SQL query such as DELETE.
+	 * Query must be in the form of a PreparedStatement.
+	 * 
+	 * @param connection	database connection object
+	 * @param query			query to be executed
+	 * @return				number of rows affected
+	 */
+	public int delete(Connection connection, PreparedStatement query) {
+		int rowsAffected = 0;
+		try {
+			rowsAffected = query.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return rowsAffected;
+	} // delete
 	
 }
